b0ddc0a9d865f4ea8b6804fa2953ae75c995d0b5
veera <veerababu9677@gmail.com>
2022-12-04 01:01:09 +0530

    Veera changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,17 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <div >
+    <h1>Hello..</h1>
+    <ul>
+      <div>Mohitha<img src='./Mohitha.jpg' alt="Mohitha" width="300" height="400"/></div>
+      <div>Jishaan <img src='./Jishaan.jpeg'  alt="Jishaan"width="400" height="400"/></div>
+      <div>Veera <img src='Veera.jpeg'  alt="Veera"width="300" height="400"/></div>
+      <div>Divya <img src='./Divya.jpeg'  alt="Divya"width="300" height="400"/></div>
+      <div>VeeraDivya <img src='./VeeraDivya.jpeg'  alt="VeeraDivya"width="500" height="400"/></div>
+      </ul>
+  </div>
   )
 }
 
